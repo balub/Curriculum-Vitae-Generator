ab2325d7de01caa774dff21131f6111b7fa59f3b
BaluB <balub_deku@juhackathon.org>
2017-06-21 01:12:06 +0530

    Able to add personal details

--- index.js
+++ index.js
@@ -30,6 +30,33 @@ form.addEventListener("submit",(e)=>{
   doc.setFontSize(20);
   doc.text(60,30, fullName);
 
+  doc.setFontSize(20);
+  doc.text(20,40, "Last Name :");
+  doc.setFontSize(20);
+  doc.text(60,40, lastName);
+
+  doc.setFontSize(20);
+  doc.text(20,50, "Email :");
+  doc.setFontSize(20);
+  doc.text(60,50, email);
+
+  doc.setFontSize(20);
+  doc.text(20,70, "phone Number :");
+  doc.setFontSize(20);
+  doc.text(60,70, phoneNum);
+
+  doc.setFontSize(20);
+  doc.text(20,80, "Address :");
+  doc.setFontSize(20);
+  doc.text(60,80, address);
+
+  doc.setFontSize(20);
+  doc.text(20,90, "LinkedIn Link :");
+  doc.setFontSize(20);
+  doc.text(60,90, linkIn);
+
+
+
   doc.save(`${fullName}.pdf`);
 
 });
